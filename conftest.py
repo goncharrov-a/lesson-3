bb7b33bc04c00a470777b7314fba35eab9035d4d
Goncharov Anton <goncharrov.a@icloud.com>
2025-10-25 21:41:36 +0300

    feat: add fixture for driver setup

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+load_dotenv()
+
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    yield driver
+    driver.quit()
